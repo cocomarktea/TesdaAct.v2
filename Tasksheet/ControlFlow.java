243bf4a61efb8ec530c4168d57d7946d00365ea7
_SabJeth <91165755+cocomarktea@users.noreply.github.com>
2024-01-31 00:59:23 +0000

    done controlflow

---
+++ Tasksheet/ControlFlow.java
@@ -0,0 +1,16 @@
+package Tasksheet;
+
+public class ControlFlow {
+    public static void main(String[] args) {
+        System.out.println("Odd and Even numbers"); // Display the string.
+
+        for(int i=1; i<=10; i++){
+            if(i%2==0){
+                System.out.println(i+" is even number!");
+            }
+            else{
+                System.out.println(i+" is odd number!");
+            }
+        };
+    }
+}
