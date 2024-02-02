d6f471a505e634d2573af0b2cf4d0fa30af2d360
_SabJeth <91165755+cocomarktea@users.noreply.github.com>
2024-02-02 14:20:28 +0000

    done array

--- Challenge/gcashBank.java
+++ Challenge/gcashBank.java
@@ -1,10 +1,23 @@
 package Challenge;
 
+import java.util.Arrays;
+
 //userAuthentication
 public class gcashBank {
-public static void main(String[] args) {
-    
-}
+    public static void main(String[] args) {
+            // Retrieve an item from an array
+        int myArray[] = new int[]{1,2,3};
+        System.out.println(myArray[0]); // Outputs 1
+
+        // Update an item in an array
+        myArray = new int[]{4, 5, 6};
+        myArray[0] = 7;
+        System.out.println(Arrays.toString(myArray)); // Outputs {7,5,6}
+
+        // clearing an array
+        myArray = null;
+        System.out.println(Arrays.toString(myArray)); // Outputs null
+    }
 
 }
 
